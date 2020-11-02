7dc394dab4b6df7b6f3f06d2325d4f911d94b19c
tdxing <123456>
2020-11-02 16:32:54 +0800

    第一次提交。

---
+++ pages/confirmOrder/confirmOrder.js
@@ -0,0 +1,197 @@
+const app = getApp();
+const GG = require('../../public/public.js');
+Page({
+	//页面的初始数据
+	data: {
+		tableNum: "",
+		confirmOrder: [],
+		// 输入框中的用餐人数
+		diner_num: 0,
+		// 用餐人数输入框获取焦点
+		diner_numF: false,
+		// 备注信息
+		remarks: "",
+		//支付方式列表
+		payWayList: [{
+			"id": 1,
+			"package": "会员卡",
+			"money": 100
+		}, {
+			"id": 2,
+			"package": "微信支付",
+			"money": 500
+		}, {
+			"id": 3,
+			"package": "银行卡",
+			"money": 1000
+		}],
+		// 购物车数据
+		cartList: [],
+		totalPrice: 0,
+		totalNum: 0,
+		// 遮罩
+		maskFlag: true,
+	},
+	// 生命周期函数--监听页面加载
+	onLoad: function(Options) {
+		var that = this;
+		let tableNum = Options.tableNum;
+		var arr = wx.getStorageSync('cart') || [];
+		for (var i in arr) {
+			this.data.totalPrice += arr[i].quantity * arr[i].price;
+			this.data.totalNum += arr[i].quantity
+		}
+		this.setData({
+			tableNum: tableNum,
+			cartList: arr,
+			totalPrice: this.data.totalPrice.toFixed(2),
+			totalNum: this.data.totalNum
+		})
+		// wx.getSystemInfo({
+		//   success: function (res) {
+		//     that.setData({
+		//       sliderLeft: (res.windowWidth / that.data.tabs.length - res.windowWidth / 2) / 2,
+		//       sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex,
+		//     });
+		//   }
+		// });
+	},
+	// 点击数字，输入框出现对应数字
+	getDinnerNUM: function(e) {
+		var dinnerNum = e.currentTarget.dataset.num;
+		var diner_num = this.data.diner_num;
+		// 点击“输”，获取焦点，
+		if (dinnerNum == 0) {
+			this.setData({
+				diner_numF: true,
+			})
+			this.getDinerNum();
+		} else {
+			this.setData({
+				diner_num: dinnerNum
+			});
+		}
+	},
+	//打开支付方式弹窗
+	choosePayWay: function() {
+		var payWayList = this.data.payWayList
+		var that = this;
+		var rd_session = wx.getStorageSync('rd_session') || [];
+
+		// 支付方式打开动画
+		var animation = wx.createAnimation({
+			duration: 200,
+			timingFunction: 'ease-in-out',
+			delay: 0
+		});
+		that.animation = animation;
+		animation.translate(0, -285).step();
+		that.setData({
+			animationData: that.animation.export(),
+		});
+		that.setData({
+			maskFlag: false,
+		});
+	},
+	// 支付方式关闭方法
+	closePayWay: function() {
+		var that = this
+		// 支付方式关闭动画
+		that.animation.translate(0, 285).step();
+		that.setData({
+			animationData: that.animation.export()
+		});
+		that.setData({
+			maskFlag: true
+		});
+	},
+	// 获取输入的用餐人数
+	getDinerNum: function(e) {
+		var diner_num = this.data.diner_num;
+		this.setData({
+			diner_num: diner_num
+		})
+	},
+	// 获取备注信息
+	getRemark: function(e) {
+		var remarks = this.data.remarks;
+		this.setData({
+			remarks: e.detail.value
+		})
+	},
+	//提交订单
+	submitOrder: function(e) {
+		var that = this;
+		var tableNum = that.data.tableNum;
+
+		var arr = wx.getStorageSync('cart') || [];
+
+		var goods_arr = [];
+		arr.forEach(order => {
+			console.log(order);
+			var goods = new Object();
+			goods.productId = order.id;
+			goods.productQuantity = order.quantity;
+			goods_arr.push(goods)
+		})
+
+		var goods_josn = JSON.stringify(goods_arr);
+		console.log(goods_josn)
+		var diner_num = this.data.diner_num; //用餐人数
+		var dinerNum;
+		var remarks = this.data.remarks; //备注信息
+		var payId = e.currentTarget.dataset.id;
+		var rd_session = wx.getStorageSync('rd_session') || [];
+		if (diner_num == 0) {
+			that.setData({
+				diner_num: 1
+			})
+		}
+		var peoples = this.data.diner_num
+		console.log("用餐人数：" + peoples)
+		console.log("备注：" + remarks)
+		console.log("桌号" + tableNum)
+		
+		
+		GG.http_GetPOst("/buyer/order/create","POST",{
+			openid: app.globalData.openid,
+			name: app.globalData.userInfo.nickName,
+			phone: "15805849785",
+			address: tableNum,
+			items: goods_josn
+		},{
+			"Content-Type": "application/x-www-form-urlencoded"
+		}).then((ret)=>{
+			// 支付方式关闭动画
+			that.animation.translate(0, 285).step();
+			that.setData({
+				animationData: that.animation.export()
+			});
+			that.setData({
+				maskFlag: true
+			});
+			wx.showToast({
+				title: '下单成功！',
+			})
+			wx.setStorageSync('cart', "")
+			wx.switchTab({
+				url: '../me/me',
+			})
+		},(err)=>{
+			// 支付方式关闭动画
+			that.animation.translate(0, 285).step();
+			that.setData({
+				animationData: that.animation.export()
+			});
+			that.setData({
+				maskFlag: true
+			});
+			//  wx.showToast({
+			//    title: res.data.message,
+			// })
+		})
+
+	},
+
+
+})
---
+++ public/public.js
@@ -0,0 +1,242 @@
+// 确认提示
+function tk_ts_showModal(bt, nr="") {
+	wx.showModal({
+		title: bt, //标题
+		content: nr,
+		showCancel: false,
+		success: function() {}, //接口调用成功的回调函数
+		fail: function() {}, //接口调用失败的回调函数
+		complete: function() {}, //接口调用结束的回调函数
+		confirm: function() {
+
+		}
+	})
+}
+// 延迟模态框
+function tk_ts_showToast(bt, icon, duration) {
+	wx.showToast({
+		title: bt, //标题
+		icon: icon,
+		duration: duration,
+		mask: true,
+		success: function() {}, //接口调用成功的回调函数
+		fail: function() {}, //接口调用失败的回调函数
+		complete: function() {}, //接口调用结束的回调函数
+		confirm: function() {
+
+		}
+	})
+}
+// 打电话
+function kfdh(dh) {
+	wx.makePhoneCall({
+		phoneNumber: dh,
+		fail: function() {
+			wx.showToast({
+				title: '无法跳转或权限不足!', //标题
+				icon: 'loading', //图标，支持"success"、"loading"
+				duration: 500, //提示的延迟时间，单位毫秒，默认：1500
+				mask: false, //是否显示透明蒙层，防止触摸穿透，默认：false
+				success: function() {}, //接口调用成功的回调函数
+				fail: function() {}, //接口调用失败的回调函数
+				complete: function() {} //接口调用结束的回调函数
+			})
+		}
+	})
+}
+// JS对象深度合并
+function deepMerge(target = {}, source = {}) {
+	target = deepClone(target);
+	if (typeof target !== 'object' || typeof source !== 'object') return false;
+	for (var prop in source) {
+		if (!source.hasOwnProperty(prop)) continue;
+		if (prop in target) {
+			if (typeof target[prop] !== 'object') {
+				target[prop] = source[prop];
+			} else {
+				if (typeof source[prop] !== 'object') {
+					target[prop] = source[prop];
+				} else {
+					if (target[prop].concat && source[prop].concat) {
+						target[prop] = target[prop].concat(source[prop]);
+					} else {
+						target[prop] = deepMerge(target[prop], source[prop]);
+					}
+				}
+			}
+		} else {
+			target[prop] = source[prop];
+		}
+	}
+	return target;
+}
+// 判断arr是否为一个数组，返回一个bool值
+function isArray(arr) {
+	return Object.prototype.toString.call(arr) === '[object Array]';
+}
+// 深度克隆
+function deepClone(obj) {
+	// 对常见的“非”值，直接返回原来值
+	if ([null, undefined, NaN, false].includes(obj)) return obj;
+	if (typeof obj !== "object" && typeof obj !== 'function') {
+		//原始类型直接返回
+		return obj;
+	}
+	var o = isArray(obj) ? [] : {};
+	for (let i in obj) {
+		if (obj.hasOwnProperty(i)) {
+			o[i] = typeof obj[i] === "object" ? deepClone(obj[i]) : obj[i];
+		}
+	}
+	return o;
+}
+// 封装请求方法
+const http_GetPOst = (url, method = 'GET', param = {}, header = {}, showLoading = "") => {
+	// 默认配置
+	const config = {
+		// 请求的本域名
+		baseUrl: 'http://localhost:8080/sell',
+		// 设置为json，返回后会对数据进行一次JSON.parse()
+		dataType: 'json',
+		// 是否显示请求中的loading
+		showLoading: true,
+		// 请求loading中的文字提示
+		loadingText: '请求中...',
+		// 在此时间内，请求还没回来的话，就显示加载中动画，单位ms
+		loadingTime: 800,
+		// 展示loading的时候，是否给一个透明的蒙层，防止触摸穿透
+		loadingMask: true,
+		// 配置请求头信息
+		header: {
+			'content-type': 'application/json;charset=UTF-8',
+		},
+	}
+
+	// 请求拦截部分，如配置，每次请求前都会执行
+	const LanJieQi_KaiShi = (config) => {
+		console.log('请求开始拦截器:', config)
+		// 取本地存储的Cookie,不为空就添上
+		try {
+			const Cookie = wx.getStorageSync('cookieKey');
+			if (Cookie) config.header.Cookie = Cookie;
+		} catch (e) {
+			console.log("取出本地cookie错误:" + e);
+		}
+
+		// 返回false,拦截不发送请求
+		return config;
+	}
+
+	// 响应拦截，如配置，每次请求结束都会执行本方法
+	const LanJieQi_JieShu = (res) => {
+		console.log('请求结束拦截器:', res)
+		if (res.statusCode == 200) {
+			try {
+				if (res.header["Set-Cookie"]) wx.setStorageSync('cookieKey', res.header["Set-Cookie"]);
+			} catch (e) {
+				console.log("存储本地cookie错误:" + e);
+			}
+			
+			// 测试使用--------------------------------开始
+			return res;
+			// 测试使用--------------------------------结束
+
+			// 判断是否正常请求到
+			if (res.data.errcode == 0) return res;
+
+			switch (parseInt(res.data.errcode)) {
+				case 999:
+					break;
+				default:
+					tk_ts_showToast(res.data.msg ? res.data.msg : '未定义错误信息', 'none', 1000)
+					return false;
+			}
+		} else {
+			switch (res.statusCode) {
+				case 999:
+					break;
+				default:
+					tk_ts_showToast(res.data.msg ? res.data.msg : '操作出错!', 'none', 1000)
+					return false;
+			}
+		}
+
+		return false;
+	}
+	
+	if (showLoading!=""){
+		config.showLoading=showLoading
+	}
+
+	let ChuLiGuoDeConfig = {}
+	ChuLiGuoDeConfig.url = config.baseUrl + url
+	ChuLiGuoDeConfig.method = method
+	ChuLiGuoDeConfig.header = Object.assign(config.header, header)
+	ChuLiGuoDeConfig.data = param
+	ChuLiGuoDeConfig.dataType = config.dataType
+	ChuLiGuoDeConfig = LanJieQi_KaiShi(ChuLiGuoDeConfig)
+
+	if (config.showLoading) {
+		var Loading = false
+		var setTimeoutID = setTimeout(() => {
+			Loading = true
+			wx.showLoading({
+				title: config.loadingText,
+				mask: config.loadingMask,
+			})
+		}, config.loadingTime)
+	}
+	return new Promise((resolve, reject) => {
+		wx.request({
+			url: ChuLiGuoDeConfig.url,
+			method: ChuLiGuoDeConfig.method,
+			header: ChuLiGuoDeConfig.header,
+			data: ChuLiGuoDeConfig.data,
+			success: (res) => {
+				if (config.dataType == 'json') {
+					try {
+						res.data = JSON.parse(res.data)
+					} catch (e) {}
+				}
+				res = LanJieQi_JieShu(res)
+				if (res == false) {
+					setTimeout(() => {
+						reject(res)
+					}, 1000)
+				} else {
+					resolve(res)
+				}
+			},
+			fail: (err) => {
+				err = LanJieQi_JieShu(err)
+				if (err == false) {
+					setTimeout(() => {
+						reject(err)
+					}, 1000)
+				} else {
+					resolve(err)
+				}
+			},
+			complete: () => {
+				if (config.showLoading) {
+					if (setTimeoutID) {
+						clearTimeout(setTimeoutID);
+						if (Loading) {
+							wx.hideLoading();
+						}
+					}
+				}
+			}
+		})
+	})
+}
+const app = getApp()
+module.exports = {
+	tk_ts_showModal: tk_ts_showModal,
+	tk_ts_showToast: tk_ts_showToast,
+	kfdh: kfdh,
+	deepMerge: deepMerge,
+	isArray: isArray,
+	deepClone: deepClone,
+	http_GetPOst: http_GetPOst,
+};
